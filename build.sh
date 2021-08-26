2a188ac5573860afc83b2d217ab3500b47c6151b
Jack E Bois <jack@flux.xyz>
2021-08-26 21:08:49 +0200

    anonymized build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 env NEAR_ENV=testnet
 
 # Register account with wNEAR contract and Oracle contract, give 20 NEAR to store with oracle to allow for multiple Data Requests to be made
-near call v2.wnear.flux-dev storage_deposit '{"account_id": "requestor.heartymoriarty.testnet"}' --accountId requestor.heartymoriarty.testnet --amount 0.00125 --gas=300000000000000
-near call 05.oracle.flux-dev storage_deposit '{"account_id": "requestor.heartymoriarty.testnet"}' --accountId requestor.heartymoriarty.testnet --amount 20 --gas=300000000000000
+near call v2.wnear.flux-dev storage_deposit '{"account_id": "YOUR_TESTNET_ACCOUNT_ID"}' --accountId YOUR_TESTNET_ACCOUNT_ID --amount 0.00125 --gas=300000000000000
+near call 05.oracle.flux-dev storage_deposit '{"account_id": "YOUR_TESTNET_ACCOUNT_ID"}' --accountId YOUR_TESTNET_ACCOUNT_ID --amount 20 --gas=300000000000000
 
 # Deposit 20 NEAR to get 20 wNEAR tokens to use in your contract
-near call v2.wnear.flux-dev near_deposit "{}" --accountId requestor.heartymoriarty.testnet --amount 20 --gas=300000000000000
+near call v2.wnear.flux-dev near_deposit "{}" --accountId YOUR_TESTNET_ACCOUNT_ID --amount 20 --gas=300000000000000
 
 # Build and deploy your requestor-sample-contract
 cargo build --target wasm32-unknown-unknown --release
 cp ./target/wasm32-unknown-unknown/release/request_interface.wasm  ./res
-near deploy requestor.heartymoriarty.testnet ./res/request_interface.wasm new --initArgs '{"oracle": "05.oracle.flux-dev", "stake_token": "v2.wnear.flux-dev"}'
+near deploy YOUR_TESTNET_ACCOUNT_ID ./res/request_interface.wasm new --initArgs '{"oracle": "05.oracle.flux-dev", "stake_token": "v2.wnear.flux-dev"}'
