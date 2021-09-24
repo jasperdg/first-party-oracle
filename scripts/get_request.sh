17c33128fb60a8e7a2586498d3ae0b9afac38905
Khang Dinh <thesephi@gmail.com>
2021-09-24 20:11:19 +0200

    updated cli prompt for get_request.sh script

--- scripts/get_request.sh
+++ scripts/get_request.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ -z "$1" ]] && echo "Expected syntax: $0 YOUR_TESTNET_ACCOUNT_ID YOUR_REQUEST_NONCE" >&2 && exit 1
-[[ -z "$2" ]] && echo "Expected syntax: $0 YOUR_TESTNET_ACCOUNT_ID YOUR_REQUEST_NONCE" >&2 && exit 1
+[[ -z "$1" ]] && echo "Expected syntax: $0 YOUR_REQUESTER_ID YOUR_REQUEST_ID" >&2 && exit 1
+[[ -z "$2" ]] && echo "Expected syntax: $0 YOUR_REQUESTER_ID YOUR_REQUEST_ID" >&2 && exit 1
 
 near call $1 get_data_request "{\"request_id\": \"$2\"}" --accountId $1
